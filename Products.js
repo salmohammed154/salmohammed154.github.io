97138039c30b8a0c16a6452c47332a5622c90e42
Tasneem <tasneemkadry63@example.com>
2026-05-08 12:28:21 +0300

    updata product page

--- Products.js
+++ Products.js
@@ -9,5 +9,30 @@
         }
 
 
+function search() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let products = document.querySelectorAll(".product");
 
+    products.forEach(product => {
+        let name = product.querySelector("p").innerText.toLowerCase();
+
+        product.style.display = name.includes(input) ? "block" : "none";
+    });
+}
+
+function setView(type) {
+    let container = document.getElementById("productsContainer");
+
+    container.classList.remove("grid", "list", "details");
+    container.classList.add(type);
+}
+function showMore() {
+    let hiddenProducts = document.querySelectorAll(".product.hidden");
+
+    hiddenProducts.forEach(product => {
+        product.classList.remove("hidden");
+    });
+
+    document.querySelector(".more").style.display = "none";
+}
  
